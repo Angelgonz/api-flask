a4f830aa612e2618a53eb799c1c9937e46da9364
Angelgonz <57816145+Angelgonz@users.noreply.github.com>
2022-07-11 23:33:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class develomentConfig():
     DEBUG=True
     MYSQL_HOST='angel.cbk7spjlzjy0.us-east-2.rds.amazonaws.com'
+    PORT='3306'
     MYSQL_USER='admin'
     MYSQL_PASSWORD= 'avefenix'
     MYSQL_DB='angel'

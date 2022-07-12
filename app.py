b57a052cb23793b4674d358417ef855ee4954d6c
Angelgonz <57816145+Angelgonz@users.noreply.github.com>
2022-07-12 17:25:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -253,9 +253,9 @@ def listar_Email():
         
         
         
-        sqlfinal="SELECT * from angel.index limit 200"
+        sqlfinal="SELECT * from angel.index limit 400"
         # Leemocreate_prep_datasets 12000 correos electrónicos
-        X, y = create_prep_dataset(sqlfinal, 200)
+        X, y = create_prep_dataset(sqlfinal, 400)
         spam=0
         inc=0
         ham=0
@@ -263,14 +263,14 @@ def listar_Email():
             #print(i)
             inc+=1
             
-            if inc>=1 and inc<=150:
+            if inc>=1 and inc<=350:
 
                 if i=="spam":
                     spam+=1
                 else:
                     ham+=1
         # Utilizamos 10000 correos electrónicos para entrenar el algoritmo y 2000 para realizar pruebas
-        X_train, y_train = X[:150], y[:150]
+        X_train, y_train = X[:350], y[:350]
         X_test, y_test = X[50:], y[50:]
         #print("-------------------este es el train")
         #print("")
